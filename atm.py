30d39315c71becaec235d62a8734d4b522cf1c99
FATEMEHRAHIMIAN <110339993+FATEMEHRAHIMIAN@users.noreply.github.com>
2022-09-03 00:51:40 +0430

    Add files via upload

--- atm.py
+++ atm.py
@@ -1,19 +1,25 @@
 correct_password = 1234
 inverse_password = 4321
-tryw = 0 
+tryw = 0
+if tryw >= 3 :
+    print("YOUR ACCOUNT IS LOCKED!")       
+else:
+    while tryw < 3:
+        admittance = int(input("Enter your password following the security instructions ..\n"))
+        if ( 999 < admittance < 10000 ):
+            if admittance == correct_password:
+                print("welcome .")
+                tryw = tryw+1
+                break
+            elif admittance == inverse_password:
+                print("The password is wrong, we called the police.. \n")
+           
+                tryw = tryw+1
 
-while tryw < 4:
-    admittance = int(input("Enter your password following the security instructions ..\n"))
-    if ( 999 < admittance < 10000 ):
-        if admittance == correct_password:
-            print("welcome .")
-            break
-        elif admittance == inverse_password:
-            print("The password is wrong, we called the police.. \n")
-
-        else:
-            print("try again..")
+            else:
+                print("try again..")
+                tryw = tryw + 1
     
-    else:
-            print("You entered less than four digits.. \n")        
-    tryw = tryw +1        
+        else:
+                print("You entered less than four digits.. \n")   
+
